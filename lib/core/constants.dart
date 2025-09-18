66e5769ec5e2719fecb77a46bbcf1497bc1bda27
Angel jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2025-09-18 14:12:47 -0600

    app bar with image logo

---
+++ lib/core/constants.dart
@@ -0,0 +1,2 @@
+const String seciLogo =
+    "https://imgs.search.brave.com/rP61Ufiye3SsvOYu507vzXdF_jKwCd8oylgqw7LIQ-M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xNTU2NC8xNTU2/NDMyOS5wbmc_c2Vt/dD1haXNfd2hpdGVf/bGFiZWw";
--- lib/features/counters/presentation/screen/counters.dart
+++ lib/features/counters/presentation/screen/counters.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seci_desktop/core/constants.dart';
 
 class CountersScreen extends StatelessWidget {
   const CountersScreen({super.key});
@@ -9,7 +10,28 @@ class CountersScreen extends StatelessWidget {
     // final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
-        title: Text("SECI", style: Theme.of(context).textTheme.titleLarge),
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          decoration: BoxDecoration(
+            color: colors.primary.withValues(alpha: 0.1),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Icon(Icons.photo_library, color: colors.primary, size: 26),
+              Image.network(seciLogo, width: 26, height: 26),
+              const SizedBox(width: 4),
+              Text(
+                "SECI",
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  color: colors.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
         backgroundColor: colors.surface,
         elevation: 3,
         // foregroundColor: colors.error,
