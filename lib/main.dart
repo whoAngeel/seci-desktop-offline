5a9c3b8d59db5a56019d551aface571cdcb47e65
azorrilla-alt <spt.azorrilla@insaite.io>
2025-09-21 02:19:28 -0600

    feat: actualizando para android

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:io';
 
 import 'package:seci_desktop/app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Configuración para desktop
-  await windowManager.ensureInitialized();
+  // Configuración solo para Windows desktop
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
 
-  final WindowOptions windowOptions = const WindowOptions(
-    // Elimina las propiedades de tamaño fijo para permitir el modo de pantalla completa
-    center: true,
-    backgroundColor: Colors.transparent,
-    // skipTaskbar: false,
-    titleBarStyle: TitleBarStyle.normal,
-    windowButtonVisibility: true,
+    final WindowOptions windowOptions = const WindowOptions(
+      // Elimina las propiedades de tamaño fijo para permitir el modo de pantalla completa
+      center: true,
+      backgroundColor: Colors.transparent,
+      // skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+      windowButtonVisibility: true,
 
-    title: 'Sistema de Entradas del Centroo de Información [SECI]',
-    alwaysOnTop: false,
-    // fullScreen: true, // Se inicia en pantalla completa
-  );
+      title: 'Sistema de Entradas del Centroo de Información [SECI]',
+      alwaysOnTop: false,
+      // fullScreen: true, // Se inicia en pantalla completa
+    );
 
-  await windowManager.waitUntilReadyToShow(windowOptions, () async {
-    // Configurar propiedades de la ventana
-    // await windowManager.setPreventClose(false);
-    // await windowManager.setResizable(false); // No redimensionable
-    await windowManager.setMinimizable(true);
-    await windowManager.setMaximizable(true); // Permitir fullscreen
-    await windowManager.setSize(const Size(1920, 1080)); // Forzar tamaño exacto
-    await windowManager.center(animate: true);
-    // await windowManager.
-    // Mostrar la ventana
-    await windowManager.show();
-    await windowManager.focus();
-  });
+    await windowManager.waitUntilReadyToShow(windowOptions, () async {
+      // Configurar propiedades de la ventana
+      // await windowManager.setPreventClose(false);
+      // await windowManager.setResizable(false); // No redimensionable
+      await windowManager.setMinimizable(true);
+      await windowManager.setMaximizable(true); // Permitir fullscreen
+      await windowManager.setSize(
+        const Size(1920, 1080),
+      ); // Forzar tamaño exacto
+      await windowManager.center(animate: true);
+      // await windowManager.
+      // Mostrar la ventana
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   runApp(const ProviderScope(child: MyApp()));
 }
