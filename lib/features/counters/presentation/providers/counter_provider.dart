84f706daf981bad524eb3433ed07c98f689abd68
Angel jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2025-09-23 10:11:01 -0600

    feat: primer release

--- lib/features/counters/presentation/providers/counter_provider.dart
+++ lib/features/counters/presentation/providers/counter_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:seci_desktop/core/constants.dart';
 import 'package:seci_desktop/core/database/database.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
