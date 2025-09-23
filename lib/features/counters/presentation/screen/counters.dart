77cf102d3ca286348a14a272ce1c739c53121376
azorrilla-alt <spt.azorrilla@insaite.io>
2025-09-22 23:35:41 -0600

    format: Formato para la ultima tarjeta de total general

--- lib/features/counters/presentation/screen/counters.dart
+++ lib/features/counters/presentation/screen/counters.dart
@@ -14,22 +14,144 @@ class CountersScreen extends ConsumerWidget {
     final counterState = ref.watch(counterProvider);
 
     return Scaffold(
-      body: Column(
+      body: Row(
         children: [
-          // Header
-          // _buildHeader(context, counterState),
-
-          // // Error message si existe
-          if (counterState.error != null)
-            // if (true)
-            _buildErrorBanner(context, ref, counterState.error!),
-
-          // // Content
+          _buildSideBar(context, ref),
           Expanded(
-            // flex: 15
-            child: counterState.isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : _buildCountersGrid(counterState),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (counterState.error != null)
+                  _buildErrorBanner(context, ref, counterState.error!),
+
+                Expanded(
+                  child: counterState.isLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : _buildCountersGrid(counterState),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSideBar(BuildContext context, WidgetRef ref) {
+    return Container(
+      width: 80,
+      // color: Colors.red,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        border: Border(
+          right: BorderSide(
+            color: Theme.of(context).colorScheme.outline.withValues(alpha: 0.1),
+            width: 1,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              border: Border(
+                bottom: BorderSide(
+                  color: Theme.of(
+                    context,
+                  ).colorScheme.outline.withValues(alpha: 0.2),
+                  width: 1,
+                ),
+              ),
+            ),
+            child: Text(
+              'SECI',
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          // Expanded()
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 16,
+                children: [
+                  // Text(
+                  //   'Activo',
+                  //   style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                  //     fontWeight: FontWeight.bold,
+                  //     color: Theme.of(context).colorScheme.primary,
+                  //   ),
+                  // ),
+                  // FloatingActionButton(onPressed: () {}),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.table_chart,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    tooltip: 'Datos de hoy',
+                    alignment: Alignment.center,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.calendar_month,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    tooltip: "Ver Calendario",
+                    alignment: Alignment.center,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.backup_table_sharp,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    tooltip: "Ver todos los datos",
+                    alignment: Alignment.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 16,
+              children: [
+                Tooltip(
+                  message: 'Sistema Activo',
+                  child: Icon(
+                    Icons.check_circle,
+                    color: Colors.green,
+                    size: 24,
+                  ),
+                ),
+                Tooltip(
+                  message: 'Elaborado por: Angel Jesus Zorrilla Cuevas',
+                  child: Icon(Icons.info, color: Colors.blue, size: 24),
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -114,26 +236,32 @@ class CountersScreen extends ConsumerWidget {
   }
 
   Widget _buildCountersGrid(CounterState state) {
-    return Padding(
+    return Center(
       // padding: const EdgeInsets.all(16),
-      padding: const EdgeInsets.symmetric(horizontal: 148, vertical: 32),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 5,
-          childAspectRatio: 1.5,
-          // mainAxisExtent: 150,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-          // mainAxisExtent: ,
+      // child: ConstrainedBox(
+      // constraints: const BoxConstraints(maxWidth: 1000, maxHeight: 600),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 16),
+        child: GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+            childAspectRatio: 1.5,
+            // mainAxisExtent: 150,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            // mainAxisExtent: ,
+          ),
+          itemCount: state.counters.length + 1,
+          itemBuilder: (context, index) {
+            if (index == state.counters.length) {
+              return SystemInfoCard();
+            }
+            return CounterCard(counter: state.counters[index]);
+          },
         ),
-        itemCount: state.counters.length + 1,
-        itemBuilder: (context, index) {
-          if (index == state.counters.length) {
-            return SystemInfoCard();
-          }
-          return CounterCard(counter: state.counters[index]);
-        },
       ),
+      // ),
     );
   }
 }
--- lib/features/counters/presentation/widgets/system_info_card.dart
+++ lib/features/counters/presentation/widgets/system_info_card.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:seci_desktop/features/counters/presentation/providers/counter_provider.dart';
 
 class SystemInfoCard extends ConsumerWidget {
   SystemInfoCard({super.key});
+  final now = DateTime.now();
+  final timeFormatter = DateFormat("HH:mm:ss");
+  final dateFormatter = DateFormat("dd/MM/yyyy");
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: const Text('Sistema'),
+    final counterState = ref.watch(counterProvider);
+
+    return Card(
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Total General',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              const SizedBox(height: 8),
+
+              Text(
+                '${counterState.totalCount}',
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 66,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(BuildContext context, IconData icon, String text) {
+    return Row(
+      children: [
+        Icon(icon, size: 16, color: Theme.of(context).colorScheme.primary),
+        const SizedBox(width: 4),
+        Flexible(
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
     );
   }
 }
